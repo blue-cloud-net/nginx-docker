d7a267f40c47621c6c2d9edcbc5389885ebdcadc
huansky <651494458@qq.com>
2026-01-08 14:49:01 +0800

    feat: 优化Docker镜像构建脚本，支持多平台构建并添加缓存机制

--- build/build-and-push-tencentyun.sh
+++ build/build-and-push-tencentyun.sh
@@ -13,21 +13,19 @@ for dockerfile in $dockerfiles; do
     version=$(basename "$(dirname "$dockerfile")");
     
     echo "Building and pushing tengine:$version...";
-    
-    # Cross compile the Dockerfile for multiple environments
-    # platforms=("linux/amd64" "linux/amd64/v2" "linux/amd64/v3" "linux/arm64" "linux/arm/v7" "linux/arm/v8");
-    # platforms=("linux/amd64" "linux/arm64" "linux/arm/v7" "linux/arm/v8");
-    platforms=("linux/amd64" "linux/amd64/v2" "linux/amd64/v3" "linux/arm64");
-    # platforms=("linux/amd64");
-    
-    # Convert platforms array to comma-separated string
-    platforms_csv=$(IFS=,; echo "${platforms[*]}");
-    echo "Building for platforms: $platforms_csv";
 
     # Build the Docker image for the specified platforms
     # Use --platform to specify the target platforms
     # Use --push to push the image directly to the repository
-    docker buildx build --platform "$platforms_csv" -f "$dockerfile" -t "$repository:$version" --push .;
+    docker buildx build \
+        --cache-from "type=registry,ref=$repository:buildcache" \
+        --cache-to "type=registry,ref=$repository:buildcache,mode=max" \
+        --platform "linux/amd64,linux/arm64" \
+        -f "$dockerfile" \
+        -t "$repository:$version" \
+        -t "$repository:$version-$(date +%Y%m%d%H%M%S)" \
+        --push \
+        .;
 
     echo "Successfully pushed tengine:$version to $repository";
 done
